80e00785ae2b72a6129206c09490ad0fa3be27c2
Golani <amir.golany@gmail.com>
2024-02-23 15:43:40 +0100

    ejs logics implemented

--- index.js
+++ index.js
@@ -20,17 +20,43 @@ app.use((req, res, next) => {
     next()
 });
 
-// r-three
+app.get('/create', (req, res) => {
 
-app.get('/create-r-three', (req, res) => {
-    res.render('create-r-three')
+    if (!req.query.r) {
+        return res.status(400).send('Missing Region! try adding ?r=one')
+    }
+
+    var props = {};
+
+    props.server = localIpAddress().split('.')[3] === '96' ? 'Big Touchscreen' : 'Small Touchscreen';
+    props.region = req.query.r;
+
+    if (req.query.r === 'one') {
+        props.template = ['Background Loop', 'Map Gaza']
+    }
+
+    else if (req.query.r === 'two') {
+        props.template = ['Background Loop', 'MAP Ukraine', 'MAP Osten', 'MAP Süden', 'MAP Awdijiwka', 'MAP Gegenoff.']
+    }
+
+    else if (req.query.r === 'three') {
+        props.template = ['Background Loop']
+    }
+
+    else {
+        props.template = ['Background Loop']
+    }
+
+    res.render('create', props)
 });
 
-app.get('/present-r-three', (req, res) => {
-    res.render('present-r-three')
+app.get('/present', (req, res) => {
+    res.render('present', {
+        region: req.query.r
+    })
 });
 
-app.post('/create-r-three', (req, res) => {
+app.post('/create', (req, res) => {
     const form = new formidable.IncomingForm({
         multiples: true,
         maxFileSize: 2 * 1024 * 1024 * 1024, // 2 GB limit
@@ -42,13 +68,13 @@ app.post('/create-r-three', (req, res) => {
         }
 
         // Create directory if it doesn't exist
-        const storagePath = path.join(__dirname, 'public', 'storage-r-three');
+        const storagePath = path.join(__dirname, 'public', `storage-r-${req.query.r}`);
         if (!fs.existsSync(storagePath)) {
             fs.mkdirSync(storagePath);
         }
 
         // Create directory for JSON file if it doesn't exist
-        const dbPath = path.join(__dirname, 'db-r-three');
+        const dbPath = path.join(__dirname, `db-r-${req.query.r}`);
         if (!fs.existsSync(dbPath)) {
             fs.mkdirSync(dbPath);
         }
@@ -92,215 +118,13 @@ app.post('/create-r-three', (req, res) => {
         // console.log('Fields:', fields);
         // console.log('Files:', files);
         var d = new Date(Date.now());
-        console.log(chalk.yellowBright(d.toString().split('GMT')[0], `r-two Page created`));
+        console.log(chalk.yellowBright(d.toString().split('GMT')[0], `Region ${req.query.r} created`));
         res.json(jsonData);
     });
 });
 
-app.get('/layout-r-three', (req, res) => {
-    const filePath = path.join(__dirname, 'db-r-three', 'layout.json');
-
-    // Read the file asynchronously
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-
-        try {
-            // Parse the file content as JSON
-            const jsonData = JSON.parse(data);
-
-            // Send the parsed JSON as the response
-            res.json(jsonData);
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-});
-
-// r-two
-
-app.get('/create-r-two', (req, res) => {
-    res.render('create-r-two')
-});
-
-app.get('/present-r-two', (req, res) => {
-    res.render('present-r-two')
-});
-
-app.post('/create-r-two', (req, res) => {
-    const form = new formidable.IncomingForm({
-        multiples: true,
-        maxFileSize: 2 * 1024 * 1024 * 1024, // 2 GB limit
-    });
-
-    form.parse(req, async (err, fields, files) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error parsing the form' });
-        }
-
-        // Create directory if it doesn't exist
-        const storagePath = path.join(__dirname, 'public', 'storage-r-two');
-        if (!fs.existsSync(storagePath)) {
-            fs.mkdirSync(storagePath);
-        }
-
-        // Create directory for JSON file if it doesn't exist
-        const dbPath = path.join(__dirname, 'db-r-two');
-        if (!fs.existsSync(dbPath)) {
-            fs.mkdirSync(dbPath);
-        }
-
-        fse.emptyDirSync(dbPath);
-        fse.emptyDirSync(storagePath);
-
-        const jsonData = [];
-
-        // Iterate over the fields and handle each set of inputs
-        for (let i = 1; fields[`name_${i}`] !== undefined; i++) {
-            const name = fields[`name_${i}`];
-            const mute = fields[`mute_${i}`];
-            const loop = fields[`loop_${i}`];
-            const ctrl = fields[`ctrl_${i}`];
-            const file = files[`file_${i}`];
-
-            if (file) {
-                const newFilePath = path.join(storagePath, file[0].originalFilename);
-
-                // Move the file to the storage directory
-                fs.renameSync(file[0].filepath, newFilePath);
-
-                // Add information to the JSON data
-                jsonData.push({
-                    name,
-                    mute,
-                    loop,
-                    ctrl,
-                    file: newFilePath,
-                });
-            }
-        }
-
-        jsonData.unshift({ lastUpdate: Date.now() })
-
-        // Create JSON file
-        const jsonFilePath = path.join(dbPath, `layout.json`);
-        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-
-        // console.log('Fields:', fields);
-        // console.log('Files:', files);
-        var d = new Date(Date.now());
-        console.log(chalk.yellowBright(d.toString().split('GMT')[0], `r-two Page created`));
-        res.json(jsonData);
-    });
-});
-
-app.get('/layout-r-two', (req, res) => {
-    const filePath = path.join(__dirname, 'db-r-two', 'layout.json');
-
-    // Read the file asynchronously
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-
-        try {
-            // Parse the file content as JSON
-            const jsonData = JSON.parse(data);
-
-            // Send the parsed JSON as the response
-            res.json(jsonData);
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-});
-
-// r-one
-
-app.get('/create-r-one', (req, res) => {
-    res.render('create-r-one')
-});
-
-app.get('/present-r-one', (req, res) => {
-    res.render('present-r-one')
-});
-
-app.post('/create-r-one', (req, res) => {
-    const form = new formidable.IncomingForm({
-        multiples: true,
-        maxFileSize: 2 * 1024 * 1024 * 1024, // 2 GB limit
-    });
-
-    form.parse(req, async (err, fields, files) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error parsing the form' });
-        }
-
-        // Create directory if it doesn't exist
-        const storagePath = path.join(__dirname, 'public', 'storage-r-one');
-        if (!fs.existsSync(storagePath)) {
-            fs.mkdirSync(storagePath);
-        }
-
-        // Create directory for JSON file if it doesn't exist
-        const dbPath = path.join(__dirname, 'db-r-one');
-        if (!fs.existsSync(dbPath)) {
-            fs.mkdirSync(dbPath);
-        }
-
-        fse.emptyDirSync(dbPath);
-        fse.emptyDirSync(storagePath);
-
-        const jsonData = [];
-
-        // Iterate over the fields and handle each set of inputs
-        for (let i = 1; fields[`name_${i}`] !== undefined; i++) {
-            const name = fields[`name_${i}`];
-            const mute = fields[`mute_${i}`];
-            const loop = fields[`loop_${i}`];
-            const ctrl = fields[`ctrl_${i}`];
-            const file = files[`file_${i}`];
-
-            if (file) {
-                const newFilePath = path.join(storagePath, file[0].originalFilename);
-
-                // Move the file to the storage directory
-                fs.renameSync(file[0].filepath, newFilePath);
-
-                // Add information to the JSON data
-                jsonData.push({
-                    name,
-                    mute,
-                    loop,
-                    ctrl,
-                    file: newFilePath,
-                });
-            }
-        }
-
-        jsonData.unshift({ lastUpdate: Date.now() })
-
-        // Create JSON file
-        const jsonFilePath = path.join(dbPath, `layout.json`);
-        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-
-        // console.log('Fields:', fields);
-        // console.log('Files:', files);
-        var d = new Date(Date.now());
-        console.log(chalk.yellowBright(d.toString().split('GMT')[0], `r-one Page created`));
-        res.json(jsonData);
-    });
-});
-
-app.get('/layout-r-one', (req, res) => {
-    const filePath = path.join(__dirname, 'db-r-one', 'layout.json');
+app.get('/layout', (req, res) => {
+    const filePath = path.join(__dirname, `db-r-${req.query.r}`, 'layout.json');
 
     // Read the file asynchronously
     fs.readFile(filePath, 'utf8', (err, data) => {
