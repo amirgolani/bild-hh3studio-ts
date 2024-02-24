ad6448a958d738a110a16594b3b8a1bd1b06c8b2
Golani <amir.golany@gmail.com>
2024-02-24 20:45:28 +0100

    select Page

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const path = require('path');
 const formidable = require('formidable');
 const fse = require('fs-extra');
 const chalk = require('chalk');
-const localIpAddress = require("local-ip-address")
+const localIpAddress = require("local-ip-address");
+const util = require('util');
 
 const app = express();
 const port = 4000;
+
+const mkdirAsync = util.promisify(fs.mkdir);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 app.use(express.json());
 
@@ -23,26 +27,26 @@ app.use((req, res, next) => {
 app.get('/create', (req, res) => {
 
     if (!req.query.r) {
-        return res.status(400).send('Missing Region! try adding ?r=one')
+        return res.render('select', {
+            server: localIpAddress().split('.')[3] === '96' ? 'Big Touchscreen' : 'Small Touchscreen',
+            change: localIpAddress().split('.')[3] === '96' ? 'Switch to the small touchscreen' : 'Switch to the big touchscreen',
+            newLink: localIpAddress().split('.')[3] === '96' ? 'http://10.29.134.46:4000/create' : 'http://10.29.134.96:4000/create',
+        })
     }
 
     var props = {};
 
     props.server = localIpAddress().split('.')[3] === '96' ? 'Big Touchscreen' : 'Small Touchscreen';
-    props.region = req.query.r;
-
+    props.region = req.query.r
     if (req.query.r === 'one') {
         props.template = ['Background Loop', 'Map Gaza']
     }
-
     else if (req.query.r === 'two') {
         props.template = ['Background Loop', 'MAP Ukraine', 'MAP Osten', 'MAP Süden', 'MAP Awdijiwka', 'MAP Gegenoff.']
     }
-
     else if (req.query.r === 'three') {
         props.template = ['Background Loop']
     }
-
     else {
         props.template = ['Background Loop']
     }
@@ -56,7 +60,7 @@ app.get('/present', (req, res) => {
     })
 });
 
-app.post('/create', (req, res) => {
+app.post('/create', async (req, res) => {
     const form = new formidable.IncomingForm({
         multiples: true,
         maxFileSize: 2 * 1024 * 1024 * 1024, // 2 GB limit
@@ -64,62 +68,68 @@ app.post('/create', (req, res) => {
 
     form.parse(req, async (err, fields, files) => {
         if (err) {
+            console.error('Error parsing the form:', err);
             return res.status(500).json({ error: 'Error parsing the form' });
         }
 
-        // Create directory if it doesn't exist
-        const storagePath = path.join(__dirname, 'public', `storage-r-${req.query.r}`);
-        if (!fs.existsSync(storagePath)) {
-            fs.mkdirSync(storagePath);
-        }
+        try {
+            const storagePath = path.join(__dirname, 'public', `storage-r-${req.query.r}`);
+            const dbPath = path.join(__dirname, `db-r-${req.query.r}`);
+            const jsonFilePath = path.join(dbPath, 'layout.json');
 
-        // Create directory for JSON file if it doesn't exist
-        const dbPath = path.join(__dirname, `db-r-${req.query.r}`);
-        if (!fs.existsSync(dbPath)) {
-            fs.mkdirSync(dbPath);
-        }
-
-        fse.emptyDirSync(dbPath);
-        fse.emptyDirSync(storagePath);
-
-        const jsonData = [];
-
-        // Iterate over the fields and handle each set of inputs
-        for (let i = 1; fields[`name_${i}`] !== undefined; i++) {
-            const name = fields[`name_${i}`];
-            const mute = fields[`mute_${i}`];
-            const loop = fields[`loop_${i}`];
-            const ctrl = fields[`ctrl_${i}`];
-            const file = files[`file_${i}`];
-
-            if (file) {
-                const newFilePath = path.join(storagePath, file[0].originalFilename);
-
-                // Move the file to the storage directory
-                fs.renameSync(file[0].filepath, newFilePath);
-
-                // Add information to the JSON data
-                jsonData.push({
-                    name,
-                    mute,
-                    loop,
-                    ctrl,
-                    file: newFilePath,
-                });
+            // Create directory if it doesn't exist
+            if (!fs.existsSync(storagePath)) {
+                await mkdirAsync(storagePath);
             }
+
+            // Create directory for JSON file if it doesn't exist
+            if (!fs.existsSync(dbPath)) {
+                await mkdirAsync(dbPath);
+            }
+
+            await fse.emptyDir(dbPath);
+            await fse.emptyDir(storagePath);
+
+            const jsonData = [];
+
+            // Iterate over the fields and handle each set of inputs
+            for (let i = 1; fields[`name_${i}`] !== undefined; i++) {
+                const name = fields[`name_${i}`];
+                const mute = fields[`mute_${i}`];
+                const loop = fields[`loop_${i}`];
+                const ctrl = fields[`ctrl_${i}`];
+                const file = files[`file_${i}`];
+
+                if (file) {
+                    const newFilePath = path.join(storagePath, file[0].originalFilename);
+
+                    // Move the file to the storage directory
+                    await fse.move(file[0].filepath, newFilePath);
+
+                    // Add information to the JSON data
+                    jsonData.push({
+                        name,
+                        mute,
+                        loop,
+                        ctrl,
+                        file: newFilePath,
+                    });
+                }
+            }
+
+            jsonData.unshift({ metadata: { lastUpdate: Date.now() } });
+
+            // Create JSON file
+            await writeFileAsync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+
+            var d = new Date(Date.now());
+            console.log(chalk.yellowBright(d.toString().split('GMT')[0], `Region ${req.query.r} created`));
+            res.json(jsonData);
+
+        } catch (error) {
+            console.error('Error processing the request:', error);
+            return res.status(500).json({ error: 'Error processing the request' });
         }
-
-        jsonData.unshift({ lastUpdate: Date.now() })
-
-        // Create JSON file
-        const jsonFilePath = path.join(dbPath, `layout.json`);
-        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-
-        // console.log('Fields:', fields);
-        // console.log('Files:', files);
-        var d = new Date(Date.now());
-        console.log(chalk.yellowBright(d.toString().split('GMT')[0], `Region ${req.query.r} created`));
-        res.json(jsonData);
     });
 });
 
